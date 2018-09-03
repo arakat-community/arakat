a794f340c84e5129b1e2a552fd9d959243d5c987
alihan94 <dogus544@gmail.com>
2018-09-03 16:24:51 +0300

    appbar oluşturulması

---
+++ arakat-frontend/src/components/appbar/index.tsx
@@ -0,0 +1,68 @@
+import Img from "@fdmg/ts-react-image";
+import {
+    AppBar as MuiAppBar,
+    Button,
+    Theme,
+    Toolbar,
+    Typography,
+    WithStyles,
+    withStyles,
+} from "@material-ui/core";
+import React from "react";
+import { IRouteGroup } from "../../common/models/route/group";
+
+const styles: any = (theme: Theme) => ({
+    flex: {
+        flexGrow : 1,
+    },
+    logo: {
+        height: theme.spacing.unit * 5,
+        width: theme.spacing.unit * 5,
+    },
+    navigation: {
+        left: theme.spacing.unit * 12,
+        position: "absolute",
+    },
+    toolbarRightItems: {
+        alignItems: "center",
+        display: "flex",
+        flexFlow: "row",
+    },
+});
+
+export interface IAppBarProps {
+    children: any;
+    routes: IRouteGroup[];
+    logoUrl: string;
+    onLogoClick: () => void;
+    title: string;
+}
+
+type PropsWithStyle = IAppBarProps & WithStyles<"flex" | "toolbarRightItems" | "logo" | "navigation">;
+
+const AppBar: React.SFC<IAppBarProps> = ({classes, ...props}: PropsWithStyle) => (
+    <MuiAppBar position = "absolute" color = "default">
+        <Toolbar>
+            <Button
+                onClick = { props.onLogoClick }
+            >
+                <Img
+                    src = { props.logoUrl }
+                    alt = "logo"
+                    className = { classes.logo }
+                />
+            </Button>
+
+            <Typography variant="title" color="inherit" className = {classes.flex}>
+                {props.title}
+            </Typography>
+
+            <div className = {classes.toolbarRightItems}>
+                {props.children}
+            </div>
+
+        </Toolbar>
+    </MuiAppBar>
+);
+
+export default withStyles(styles, {withTheme : true})(AppBar);
--- arakat-frontend/src/views/test/index.tsx
+++ arakat-frontend/src/views/test/index.tsx
@@ -1,9 +1,22 @@
 import { Typography } from "@material-ui/core";
 import React from "react";
 import { FormattedMessage } from "react-intl";
+import AppBar from "../../containers/appbar";
+import { routes as dashboardRoutes } from "../../routes/dashboard";
 
 const TestView: React.SFC = () => (
-    <Typography variant="title"><FormattedMessage id="example.hello.world" /></Typography>
+    <div>
+            <AppBar
+                routes = { dashboardRoutes }
+                logoUrl = { "/assets/images/logo.png" }
+                onLogoClick = { () => alert("sa")}
+                title = {"ARAKAT"}
+            >
+                <button>SA</button>
+            </AppBar>
+            <Typography variant="title"><FormattedMessage id="example.hello.world" /></Typography>
+    </div>
+
 );
 
 export default TestView;
