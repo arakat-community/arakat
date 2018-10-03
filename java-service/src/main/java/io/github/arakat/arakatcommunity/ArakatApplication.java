b6a6d9c2437afbc5a0e76d71bb5b60256cd63056
CemalUnal <cemalunal@yahoo.com>
2018-10-03 20:46:38 +0300

    Arakat Java servisi icin ilk yapi olusturuldu, ilgili servisin projesi ile ilgili paylasilmak istenmeyen dosyalar .gitignore'a eklendi.

---
+++ java-service/src/main/java/io/github/arakat/arakatcommunity/ArakatApplication.java
@@ -0,0 +1,12 @@
+package io.github.arakat.arakatcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArakatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArakatApplication.class, args);
+	}
+}
